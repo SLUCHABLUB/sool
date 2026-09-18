5a28f49b7c9b019c2a59643c55b39de12d856d10
Nelli Skogman <nelli.skogman@gmail.com>
2026-09-18 17:35:09 +0200

    Add lints

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,10 @@
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
     application
+
+    id("net.ltgt.errorprone") version "5.1.1"
+    id("com.diffplug.spotless") version "8.10.2"
 }
 
 repositories {
@@ -8,8 +13,13 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    errorprone("com.google.errorprone:error_prone_core:2.50.0")
+    compileOnly("com.google.errorprone:error_prone_annotations:2.50.0")
+
+    errorprone("com.uber.nullaway:nullaway:0.14.1")
+    implementation("org.jspecify:jspecify:1.0.0")
 }
 
 java {
@@ -20,6 +30,32 @@ java {
 
 application {
     mainClass = "Main"
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-Xlint:all,-serial")
+    options.compilerArgs.add("-Werror")
+
+    options.errorprone {
+        allDisabledChecksAsWarnings = true
+
+        error("NullAway")
+        option("NullAway:OnlyNullMarked", true)
+        option("NullAway:JSpecifyMode", true)
+
+        disable("AddNullMarkedToClass") // We have "AddNullMarkedToPackageInfo".
+        disable("SystemOut")
+        disable("DefaultPackage") // Only for the Main class
+    }
+}
+
+spotless {
+    java {
+        googleJavaFormat("1.36.1")
+            .aosp()
+            .reflowLongStrings()
+            .reorderImports(true)
+    }
 }
 
 tasks.named<Test>("test") {
